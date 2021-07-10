2927eafa4ec69a1d4e3eb3cb62b6832057d1592b
kagemeka <kagemeka1@gmail.com>
2021-07-10 14:38:37 +0900

    enhance: add python files (#3)

---
+++ py/src/atcoder/abc026/d_0.py
@@ -0,0 +1,34 @@
+def readline():
+  import sys 
+  return sys.stdin.buffer \
+    .readline().rstrip()
+
+
+def readline_ints():
+  *ints, = map(
+    int, 
+    readline().split(),
+  )
+  return ints
+
+
+def solve(a, b, c):
+  from scipy.optimize import brenth
+  import numpy as np
+
+  def f(t):
+    nonlocal a, b, c
+    h = a*t + b*np.sin(c*t*np.pi) \
+      - 100
+    return h
+  
+  print(brenth(f, a=0, b=200))
+
+
+def main():
+  a, b, c = readline_ints()
+  solve(a, b, c)
+
+
+if __name__ == '__main__':
+  main()
