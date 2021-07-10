be42079b7a2f34daa3a71f7b9e4e665ff86a0296
kagemeka <kagemeka1@gmail.com>
2021-07-10 15:08:27 +0900

    enhance: add typescript files (#15)

---
+++ ts/src/atcoder/abc002/a_0.ts
@@ -0,0 +1,120 @@
+'use strict';
+
+
+
+class IO {
+
+  private fs: (
+    any
+  ) = require(
+    'fs',
+  )
+
+  chunks: Generator;
+
+
+  private readStdin(): (
+    string
+  ) {
+    const fs = this.fs
+    return fs.readFileSync(
+      '/dev/stdin',
+      'utf8',
+    );
+  }
+
+
+  private toChunks(
+    s: string,
+  ): (
+    string[]
+  ) {
+    return (
+      s.trim().split(/ |\n/)
+    );
+  }
+  
+
+  private *toGen(
+    chunks: string[],
+  ): (
+    Generator
+  ) {
+    for (var c of chunks) {
+      yield c;
+    }
+  }
+
+
+  constructor() {
+    const fs = this.fs;
+    const chunks: (
+      string[]
+    ) = this.toChunks(
+      this.readStdin(),
+    );
+    this.chunks = this.toGen(
+      chunks,
+    );
+  }
+
+
+  read(): string {
+    return (
+      this.chunks.next().value
+    );
+  }
+
+
+  readInt(): number {
+    return +this.read();
+  }
+}
+
+
+
+abstract class Solver {
+
+  io: IO;
+
+
+  constructor() {
+    this.io = new IO();
+  }
+
+
+  abstract prepare(): void;
+
+
+  abstract solve(): void;
+
+
+  run(): void {
+    this.prepare();
+    this.solve();
+  }
+}
+
+
+
+class Problem
+extends Solver {
+
+
+  prepare(): void {
+    const io = this.io; 
+  }
+
+
+  solve(): void {
+  }
+}
+
+
+
+function main() {
+  new Problem().run();
+}
+
+
+main()
