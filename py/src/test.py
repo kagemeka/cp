315b9993c02114275c5afec8aca1a1d2bab95f70
kagemeka <kagemeka1@gmail.com>
2021-07-19 09:02:28 +0900

    enhance: solve arc123 a, b, c

--- py/src/test.py
+++ py/src/test.py
@@ -1,94 +1,68 @@
 
-# sieve of eratosthenes
-import numpy as np 
 import typing
-
+import numpy as np
 import numba as nb
 
 
-@nb.njit
-def combinations(
-  n: int,
-  r: int,
-) -> np.array:
-  a = np.arange(n)
-  ls = []
-  if r < 0 or r > n: return np.array(ls)
-  rng = np.arange(r)[::-1]
-  i = np.arange(r)
-  ls.append(list(a[:r]))
-  while 1:
-    for j in rng:
-      if i[j] != j + n - r:
-        break
-    else: return np.array(ls)
-    i[j] += 1
-    for j in range(j + 1, r):
-      i[j] = i[j - 1] + 1
-    b = []
-    for j in i: b.append(a[j])
-    ls.append(b)
-
-
-
-
-
-# TODO cut below
-
 import typing
+
+
+
+
+
+class NextPermutation():
+  def __call__(
+    self,
+    a: typing.List[int],
+  ) -> typing.Optional[
+    typing.List[int]
+  ]:
+    n = len(a)
+    i = -1
+    for j in range(
+      n - 2, -1, -1,
+    ):
+      if a[j] >= a[j + 1]:
+        continue
+      i = j
+      break
+    if i == -1: return None
+    a[i + 1:] = a[i + 1:][::-1]
+    for j in range(i + 1, n):
+      if a[i] >= a[j]: continue
+      a[i], a[j] = a[j], a[i]
+      break
+    return a
+
+
+import math
 
 
 
 class Permutations():
   def __call__(
     self,
-    a: typing.Iterable[
-      typing.Any,
-    ],
-    r: int,
+    n: int,
   ) -> typing.AsyncIterator[
-    typing.Tuple[typing.Any],
+    typing.Tuple[int]
   ]:
-    a = tuple(a)
-    n = len(a)
-    if r < 0 or r > n: return
-    rng = range(r)
-    i = list(range(n))
-    c = list(rng)
-    yield a[:r]
-    while 1:
-      for j in reversed(rng):
-        c[j] += 1
-        if c[j] == n:
-          i[j:] = (
-            i[j + 1:]
-            + i[j:j + 1]
-          )
-          c[j] = j
-          continue
-        k = c[j]
-        i[j], i[k] = i[k], i[j]
-        yield tuple(
-          a[j] for j in i[:r]
-        )
-        break
-      else: return
-
-
+    fn = NextPermutation()
+    a = list(range(n))
+    m = math.factorial(n)
+    for _ in range(m):
+      yield a; a = fn(a)
 
 
 
 def test():
-  # a = np.arange(5)
-  # for i in range(1):
-  #   c = combinations(20, 10)
-  # print(c)
-  fn = Permutations()
-  for p in fn(range(5), 3):
-    print(p)
-  ...
+
+  a = Permutations()(10)
+  print(a)
+  for x in a:
+    pass 
+  ... 
+
 
 
 if __name__ == '__main__':
   test()
-
