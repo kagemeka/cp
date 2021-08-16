c4d0c888b596a6ba014263686ceb6bb0f3f68a95
kagemeka <kagemeka1@gmail.com>
2021-08-16 13:51:19 +0900

    enhance: implement SA-iA(AtCoder ABC213 F

--- py/src/atcoder/abc213/f/sol_0.py
+++ py/src/atcoder/abc213/f/sol_0.py
@@ -1,9 +1,4 @@
-import typing 
-
-
-
-# TODO implement SA-IS (Suffix Array with Induced Sorting)
-
+import typing
 
 
 
@@ -12,31 +7,42 @@ class SAIS():
     self,
     a: typing.List[int],
   ) -> typing.List[int]:
-    assert all(0 < x < self.__m for x in a)
-    a.append(0)
-    n = len(a)
     self.__a = a
-    
-    is_s = [True] * n
-    for i in range(n - 2, -1, -1):
-      is_s[i] = (
-        is_s[i + 1] if a[i] == a[i + 1] else 
-        a[i] < a[i + 1]
-      )
-    
-    is_lms = [
-      is_s[i] and not is_s[i - 1] 
-      for i in range(n)
-    ]
-    lms = [i for i in range(n) if is_lms[i]]
-
-    self.__is_s = is_s
-    self.__lms = lms
-
-    self.__make_bucket()
+    self.__preprocess()
     self.__induce()
-    return self.__sa
+    self.__correct_lms_order()
+    self.__induce()
+    return self.__sa[1:]
 
+  
+  def __correct_lms_order(
+    self,
+  ) -> typing.NoReturn:
+    is_lms = self.__is_lms
+    lms_idx = [i for i in self.__sa if is_lms[i]]
+    a = self.__a 
+    n = len(a)
+    m = len(lms_idx)
+    na = [-1] * n
+    na[-1] = i = 1
+    for j in range(m - 1):
+      j, k = lms_idx[j], lms_idx[j + 1]
+      for d in range(n):
+        j_is_lms = is_lms[j + d]
+        k_is_lms = is_lms[k + d]
+        if a[j + d] != a[k + d] or j_is_lms ^ k_is_lms: 
+          i += 1; break
+        if d > 0 and j_is_lms | k_is_lms: break
+      na[k] = i
+    na = [i for i in na if i > 0]
+    if i == m:
+      lms_order = [-1] * m
+      for i, j in enumerate(na):
+        lms_order[j - 1] = i
+    else:
+      lms_order = SAIS()(na)
+    self.__lms = [self.__lms[i] for i in lms_order]
+     
 
   def __induce(
     self,
@@ -79,13 +85,6 @@ class SAIS():
       sa[sa_index[x]] = i
 
 
-  def __init__(
-    self,
-    max_val: int = 1 << 8,
-  ) -> typing.NoReturn:
-    self.__m = max_val
-
-
   def __make_bucket(
     self,
   ) -> typing.NoReturn:
@@ -95,21 +94,56 @@ class SAIS():
     self.__b = b
 
   
+  def __preprocess(
+    self,
+  ) -> typing.NoReturn:
+    a = self.__a
+    if min(a) <= 0:
+      a = [x + 1 for x in a]
+    assert all(x > 0 for x in a)
+    self.__m = max(a) + 1
+    a.append(0)
+    n = len(a)
+    self.__a = a
+
+    is_s = [True] * n
+    for i in range(n - 2, -1, -1):
+      is_s[i] = (
+        is_s[i + 1] if a[i] == a[i + 1] else 
+        a[i] < a[i + 1]
+      )
+
+    is_lms = [is_s[i] and not is_s[i - 1] for i in range(n)]
+    lms = [i for i in range(n) if is_lms[i]]
+
+    self.__is_s = is_s
+    self.__is_lms = is_lms 
+    self.__lms = lms
+    self.__make_bucket()
+
+
   def __set_lms(
     self,
   ) -> typing.NoReturn:
     sa_index = self.__b.copy()
     for i in range(self.__m - 1):
       sa_index[i + 1] += sa_index[i]
-    for i in self.__lms:
+    for i in self.__lms[::-1]:
       x = self.__a[i]
       sa_index[x] -= 1
       self.__sa[sa_index[x]] = i
 
 
 
+class LCPArray():
+  def __call__(
+    self,
+  ) -> typing.NoReturn:
+    ... 
+  
+
 class SuffixArray():
-  ... 
+  ...
 
 
 
