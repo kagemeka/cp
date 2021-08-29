bc9bd01c1fdbae1462885417dd63e5aee4c652bc
kagemeka <kagemeka1@gmail.com>
2021-08-29 19:14:15 +0900

    enhance: solve LeetCode weekly256 D

---
+++ py/src/leetcode/new/d/sol_0.py
@@ -0,0 +1,14 @@
+class Solution:
+  def numberOfUniqueGoodSubsequences(
+    self, 
+    binary: str,
+  ) -> int:
+    mod = 10 ** 9 + 7
+    a = [int(d) for d in binary]
+
+    dp = [0, 0]
+    for d in a:
+      dp[d] = sum(dp) + d  
+      dp[d] %= mod 
+    res = sum(dp) + (0 in a)
+    return res % mod
