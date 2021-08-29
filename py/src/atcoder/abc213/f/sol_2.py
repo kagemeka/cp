e4065bbe787fcfa4f3eb7ad3da158b026aa3ae1c
kagemeka <kagemeka1@gmail.com>
2021-08-29 15:38:03 +0900

    enhance: solve ALPC F with non-recursive SA-IS with pypy

--- py/src/atcoder/abc213/f/sol_2.py
+++ py/src/atcoder/abc213/f/sol_2.py
@@ -4,38 +4,25 @@ import sys
 import numba as nb
 
 
-@nb.njit((nb.i8[:], ))
-def sa_is(
-  a: np.array,
-) -> np.array:
-  if a.min() <= 0: a += 1
-  assert (a > 0).all()
-  a = np.hstack((a, np.array([0])))
-  n, m = a.size, a.max() + 1
-
-  is_s = np.ones(n, dtype=np.bool8)
-  for i in range(n - 1, 0, -1):
-    is_s[i - 1] = (
-      is_s[i] if a[i - 1] == a[i] else
-      a[i - 1] < a[i] 
-    )
-  is_lms = np.zeros(n, dtype=np.bool8)
-  is_lms[np.arange(1, n)[~is_s[:-1] & is_s[1:]]] = True
+@nb.njit
+def _induce(
+  a: np.ndarray,
+  is_s: np.ndarray,
+  lms: np.ndarray,
+  bucket: np.ndarray,
+) -> np.ndarray:
+  n, m = a.size, bucket.size
+  sa = np.full(n, -1, np.int32)
   
-  lms = np.flatnonzero(is_lms)
-
-  b = np.zeros(m, dtype=np.int64)
-  for x in a: b[x] += 1
-
-  def _induce():
-    sa = np.full(n, -1, dtype=np.int64)
-    sa_idx = b.cumsum()
+  def _set_lms():
+    sa_idx = bucket.cumsum()
     for i in lms[::-1]:
-      x = a[i]
+      x = a[i] 
       sa_idx[x] -= 1
       sa[sa_idx[x]] = i
-    
-    sa_idx = b.copy()
+  
+  def _induce_l():
+    sa_idx = bucket.copy()
     s = 0 
     for i in range(m):
       s, sa_idx[i] = s + sa_idx[i], s
@@ -46,63 +33,105 @@ def sa_is(
       sa[sa_idx[x]] = i
       sa_idx[x] += 1
 
-    sa_idx = b.cumsum()
+  def _induce_s():
+    sa_idx = bucket.cumsum()
     for i in range(n - 1, -1, -1):
       i = sa[i] - 1
       if i < 0 or not is_s[i]: continue
-      x = a[i]
+      x = a[i] 
       sa_idx[x] -= 1
       sa[sa_idx[x]] = i
+  
+  _set_lms()
+  _induce_l()
+  _induce_s()
+  return sa
+  
 
-    return sa
+@nb.njit
+def _preprocess(
+  a: np.ndarray
+) -> typing.Tuple[np.ndarray]:
+  n = a.size
+  is_s = np.ones(n, np.bool8)
+  for i in range(n - 1, 0, -1):
+    is_s[i - 1] = (
+      is_s[i] if a[i - 1] == a[i] else
+      a[i - 1] < a[i]
+    )
+  is_lms = np.zeros(n, np.bool8)
+  is_lms[np.arange(1, n)[~is_s[:-1] & is_s[1:]]] = True
+  lms = np.flatnonzero(is_lms)
+  bucket = np.bincount(a)
+  return is_s, is_lms, lms, bucket
+  
+
+@nb.njit
+def _compute_next_array(
+  a: np.ndarray, 
+  sa: np.ndarray, 
+  is_lms: np.ndarray,
+) -> np.ndarray:
+  n = a.size
+  lms_idx = sa[is_lms[sa]]
+  l = lms_idx.size
+  na = np.full(n, -1, dtype=np.int64)
+  na[-1] = i = 0
+  for j in range(l - 1):
+    j, k = lms_idx[j], lms_idx[j + 1]
+    for d in range(n):
+      j_is_lms = is_lms[j + d]
+      k_is_lms = is_lms[k + d]
+      if a[j + d] != a[k + d] or j_is_lms ^ k_is_lms: 
+        i += 1; break
+      if d > 0 and j_is_lms | k_is_lms: break
+    na[k] = i
+  na = na[na >= 0]
+  return na
 
 
-  sa = _induce()
+@nb.njit
+def sa_is(
+  a: np.ndarray,
+) -> np.ndarray:
+  if a.min() <= 0: a += 1
+  assert (a > 0).all()
+  a = np.hstack((a, np.array([0])))
 
+  st: typing.List[typing.Tuple[np.ndarray]] = []
+  while True:
+    is_s, is_lms, lms, b = _preprocess(a)
+    sa = _induce(a, is_s, lms, b)
+    st.append((a, is_s, lms, b))
+    a = _compute_next_array(a, sa, is_lms)
+    l = lms.size
+    if a.max() < lms.size - 1: continue
+    lms_order = np.empty(l, np.int32)
+    for i in range(l): lms_order[a[i]] = i
+    break
 
-  def _correct_lms_order():
-    nonlocal lms
-    lms_idx = sa[is_lms[sa]]
-    l = lms_idx.size
-    na = np.full(n, -1, dtype=np.int64)
-    na[-1] = i = 1
-    for j in range(l - 1):
-      j, k = lms_idx[j], lms_idx[j + 1]
-      for d in range(n):
-        j_is_lms = is_lms[j + d]
-        k_is_lms = is_lms[k + d]
-        if a[j + d] != a[k + d] or j_is_lms ^ k_is_lms: 
-          i += 1; break
-        if d > 0 and j_is_lms | k_is_lms: break
-      na[k] = i
-    na = na[na > 0]
-    if i == l:
-      lms_order = np.full(l, -1, dtype=np.int64)
-      for i in range(l):
-        lms_order[na[i] - 1] = i
-    else:
-      lms_order = sa_is(na)
+  while st:
+    a, is_s, lms, b = st.pop()
     lms = lms[lms_order]
+    lms_order = _induce(a, is_s, lms, b)
+
+  return lms_order[1:]
 
 
-  _correct_lms_order()
-  sa = _induce()
-  return sa[1:]
 
-
-@nb.njit((nb.i8[:], nb.i8[:]))
-def kasai(
+@nb.njit
+def lcp_kasai(
   a: np.array,
   sa: np.array,
 ) -> np.array:
   n = a.size
   assert n > 0 and sa.size == n
-  rank = np.zeros(n, dtype=np.int64)
-  rank[sa] = np.arange(n)
-  h = np.zeros(n - 1, dtype=np.int64)
-  l = 0
+
+  rank = np.empty(n, np.int32)
+  for i in range(n): rank[sa[i]] = i
+  h, l = np.empty(n - 1, np.int32), 0
   for i in range(n):
-    if l > 0: l -= 1
+    if l: l -= 1
     r = rank[i]
     if r == n - 1: continue
     j = sa[r + 1]
@@ -122,7 +151,7 @@ def solve(
 ) -> typing.NoReturn:
   n = a.size
   sa = sa_is(a)
-  lcp = kasai(a, sa)
+  lcp = lcp_kasai(a, sa)
 
   a = np.arange(n, 0, -1)
   for i in range(2):
--- py/src/atcoder/practice2/i/README.md
+++ py/src/atcoder/practice2/i/README.md
@@ -7,6 +7,7 @@
 - LCP Array (Longest Common Prefix Array) 
 - SA-IS
 - SA-Doubling
+- LCP Kasai
 - string
 - math
 - greedy
@@ -23,3 +24,35 @@
 # similar 
 - [Library Checker Number of Substrings](https://judge.yosupo.jp/problem/number_of_substrings)
 - [ABC213 F - Common Prefixes](https://atcoder.jp/contests/abc213/tasks/abc213_f)
+
+
+
+## sol_0
+- SA-IS (recursive)
+- LCP Kasai
+- pypy
+
+
+## sol_1
+- SA-IS (non-recursive)
+- LCP Kasai
+- numba (JIT)
+
+
+## sol_2
+- SA-IS (non-recursive)
+- LCP Kasai
+- numba (JIT, another implementation)
+
+
+## sol_3
+- SA-IS (recursive)
+- LCP Kasai
+- numba (JIT)
+
+
+
+## sol_4
+- SA-IS (non-recursive)
+- LCP Kasai
+- pypy 
