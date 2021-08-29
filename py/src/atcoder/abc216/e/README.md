1c6aa8fc5423a26eac3b8eea91c0ed5f176b516c
kagemeka <kagemeka1@gmail.com>
2021-08-29 23:16:49 +0900

    enhance: solve ABC216 A-E

---
+++ py/src/atcoder/abc216/e/README.md
@@ -0,0 +1,25 @@
+# [E - Amusement Park](https://atcoder.jp/contests/abc216/tasks/abc216_e)
+
+
+# keywords 
+- greedy 
+- binary search 
+- scanline algorithm 
+
+
+
+# summary
+## scanline
+- $O(N\log{N})$
+- sort A in descending order
+- sol_0
+
+
+
+## binary search
+- 
+
+
+
+- similar 
+  - [ABC149 E - Handshake](https://atcoder.jp/contests/abc149/tasks/abc149_e)
--- py/src/test.py
+++ py/src/test.py
@@ -4,68 +4,24 @@ import numpy as np
 
 
 
+T = typing.TypeVar('T')
 @nb.njit
-def mod_matrix_dot(
-  a: np.ndarray,
-  b: np.ndarray,
-  mod: int,
-) -> np.ndarray:
-  r, c = a.shape
-  h, w = b.shape
-  assert c == h
-  c = np.zeros(
-    (r, w), 
-    dtype=np.int64,
-  )
-  for i in range(r):
-    for j in range(w):
-      c[i, j] = np.sum(a[i] * b[:, j] % mod) % mod 
-  return c
-      
-
-# @nb.njit
-# def mod_matrix_pow(
-#   a: np.ndarray,
-#   n: int,
-#   mod: int,
-# ) -> np.ndarray:
-#   if n == 0:
-#     return np.eye(
-#       a.shape[0],
-#       dtype=np.int64,
-#     )  
-#   x = mod_matrix_pow(a, n >> 1, mod)
-#   x = mod_matrix_dot(x, x, mod)
-#   if n & 1: 
-#     x = mod_matrix_dot(x, a, mod)
-#   return x
-
-
-@nb.njit
-def mod_matrix_pow(
-  a: np.ndarray,
-  n: int,
-  mod: int,
-) -> np.ndarray:
-  x = np.eye(
-    a.shape[0],
-    dtype=np.int64,
-  )
-  while n:
-    if n & 1:
-      x = mod_matrix_dot(x, a, mod)
-    x = mod_matrix_dot(x, x, mod)
-    n >>= 1
-  return x
+def heappush(
+  h: typing.List,
+  x: T,
+  less: typing.Callable[[T, T], bool],
+) -> typing.NoReturn:
+  
+  ... 
 
 
 @nb.njit(
   cache=True,
 )
 def test():
-  a = np.arange(1, 10).reshape(3, 3)
-  mod = 998244353
-  print(mod_matrix_pow(a, 2, mod))
+  h = [1]
+  heappush(h, 1, lambda x, y: x < y)
+  
 
 
 if __name__ == '__main__':
