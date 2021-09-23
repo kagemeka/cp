bc98f24db124048b76693424c20721ed5df428c6
kagemeka <kagemeka1@gmail.com>
2021-09-24 07:10:41 +0900

    enhance: solve ABC007

---
+++ py/src/atcoder/abc007/README.md
@@ -0,0 +1,1 @@
+# [AtCoder Beginner Contest 007](https://atcoder.jp/contests/abc007)
---
+++ py/src/atcoder/abc007/b/sol_1.py
@@ -0,0 +1,11 @@
+import typing 
+
+
+def main() -> typing.NoReturn:
+  s = input()
+  if s == 'a':
+    print(-1)
+  else:
+    print('a')
+
+main()
