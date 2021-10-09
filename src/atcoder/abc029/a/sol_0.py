481d9d568a07690a073a484095fbb64320d265b7
kagemeka <kagemeka1@gmail.com>
2021-10-09 17:50:53 +0900

    enhance: solve ABC028, 029, 030, 031, 032, 137, 140E

---
+++ src/atcoder/abc029/a/sol_0.py
@@ -0,0 +1,9 @@
+import typing 
+
+
+
+def main() -> typing.NoReturn:
+  print(input() + 's')
+
+
+main()
---
+++ src/atcoder/abc031/a/sol_0.py
@@ -0,0 +1,10 @@
+import typing 
+
+
+
+def main() -> typing.NoReturn:
+  a, d = map(int, input().split())
+  print((a + 1) * d if a <= d else a * (d + 1))
+
+
+main()
