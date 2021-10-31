f6045c6ecb0f71ad3c7d72b01b77afb21abfff80
kagemeka <kagemeka1@gmail.com>
2021-10-31 09:41:24 +0900

    enahce: solve AOJ GRL_1_C with johnson_dense (rust)

--- src/aoj/grl/1/c/sol_1.rs
+++ src/aoj/grl/1/c/sol_1.rs
@@ -17,6 +17,7 @@ impl<R: std::io::Read> Scanner<R> {
 }
 
 
+
 // #[allow(warnings)]
 fn main() {
     use std::io::Write;
@@ -36,7 +37,7 @@ fn main() {
         g[s][t] = std::cmp::min(g[s][t], d);
     }
     for i in 0..n { g[i][i] = 0; }
-    if let Ok(dist) = floyd_warshall(g) {
+    if let Ok(dist) = johnson_dense(&g) {
        for i in 0..n {
            for j in 0..n {
                if dist[i][j] == inf { 
@@ -50,6 +51,58 @@ fn main() {
     } else {
         writeln!(out, "NEGATIVE CYCLE").unwrap();
     }
+}
+
+
+/// O(V^3)
+pub fn johnson_dense(g: &Vec<Vec<i64>>) -> Result<Vec<Vec<i64>>, NegativeCycleError> {
+    let n = g.len();
+    let inf = std::i64::MAX;
+    let mut t = g.to_vec();
+    t.push(vec![0; n + 1]);
+    for i in 0..n { t[i].push(inf); }
+    let h = bellman_ford_dense(&t, n)?;
+    t = g.to_vec();
+    for u in 0..n {
+        for v in 0..n {
+            if t[u][v] != inf { t[u][v] += h[u] - h[v]; }
+        }
+    }
+    let mut dist = Vec::with_capacity(n);
+    for i in 0..n {
+        let mut d = dijkstra_dense(&t, i);
+        for j in 0..n { 
+            if d[j] != inf { d[j] -= h[i] - h[j]; } 
+        }
+        dist.push(d);
+    }
+    Ok(dist)
+}
+
+
+pub fn dijkstra_dense(g: &Vec<Vec<i64>>, src: usize) -> Vec<i64> {
+    let n = g.len();
+    let inf = std::i64::MAX;
+    let mut dist = vec![inf; n];
+    dist[src] = 0;
+    let mut visited = vec![false; n];
+    loop {
+        let mut u = -1;
+        let mut du = inf;
+        for i in 0..n {
+            if !visited[i] && dist[i] < du { u = i as i32; du = dist[i]; }
+        }
+        if u == -1 { break; }
+        let u = u as usize;
+        visited[u] = true;
+        for v in 0..n {
+            if g[u][v] == inf { continue; }
+            let dv = du + g[u][v];
+            if dv >= dist[v] { continue; }
+            dist[v] = dv;
+        }
+    }
+    dist
 }
 
 
@@ -75,19 +128,25 @@ impl std::error::Error for NegativeCycleError {
 }
 
 
-pub fn floyd_warshall(mut g: Vec<Vec<i64>>) -> Result<Vec<Vec<i64>>, NegativeCycleError> {
-    let inf = std::i64::MAX;
+/// O(V^3)
+pub fn bellman_ford_dense(g: &Vec<Vec<i64>>, src: usize) -> Result<Vec<i64>, NegativeCycleError> {
     let n = g.len();
-    for k in 0..n {
-        for i in 0..n {
-            for j in 0..n {
-                if g[i][k] == inf || g[k][j] == inf { continue; }
-                g[i][j] = std::cmp::min(g[i][j], g[i][k] + g[k][j]);
+    let inf = std::i64::MAX;
+    let mut dist = vec![inf; n];
+    dist[src] = 0;
+    for _ in 0..n - 1 {
+        for u in 0..n {
+            for v in 0..n {
+                if dist[u] == inf || g[u][v] == inf || dist[u] + g[u][v] >= dist[v] { continue; }
+                dist[v] = dist[u] + g[u][v];
             }
         }
     }
-    for i in 0..n {
-        if g[i][i] < 0 { return Err(NegativeCycleError::new()); }
+    for u in 0..n {
+        for v in 0..n {
+            if dist[u] == inf || g[u][v] == inf || dist[u] + g[u][v] >= dist[v] { continue; }
+            return Err(NegativeCycleError::new());
+        }
     }
-    Ok(g)
+    Ok(dist)
 }
