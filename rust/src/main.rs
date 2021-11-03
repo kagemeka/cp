557096983c8e5f2a4399ca9bd691e7ff77c769b2
kagemeka <kagemeka1@gmail.com>
2021-11-03 13:27:33 +0900

    enhance: solve AOJ GRL_5_B (rust)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -25,9 +25,8 @@ fn main() {
     let stdout = std::io::stdout();
     let out = &mut std::io::BufWriter::new(stdout.lock());  
 
-    let inf = std::i64::MAX;
+    // let inf = std::i64::MAX;
     let n: usize = sc.scan();
-    // let m: usize = sc.scan();
     let mut g: Vec<(usize, usize, i64)> = Vec::with_capacity(n - 1);
     for _ in 0..n - 1 {
         let s: usize = sc.scan();
@@ -35,42 +34,86 @@ fn main() {
         let w: i64 = sc.scan();
         g.push((s, t, w));
     }
-    let (path, diameter) = tree_diameter(&g);
-    writeln!(out, "{}", diameter).unwrap();
-} 
+    let m = Monoid::<i64> { 
+        op: Box::new(|x, y| std::cmp::max(*x, *y)) ,
+        e: Box::new(|| 0),
+        commutative: true,
+    };
+    let map = |x: &i64, y: &i64| x + y;
+    let dp = rerooting(&g, &m, Box::new(map));
+    for x in dp {
+        writeln!(out, "{}", x).unwrap();
+    }
+}
 
 
+// Fn(&S, &S) -> S is a trait.
+/// this is a dynamic size object at compilation time.
+/// thus, it's needed to be enclosed with Box<dyn> pointer.
+pub struct Monoid<S> {
+    pub op: Box<dyn Fn(&S, &S) -> S>,
+    pub e: Box<dyn Fn() -> S>,
+    pub commutative: bool,
+}
 
-pub fn tree_diameter(g: &Vec<(usize, usize, i64)>) -> (Vec<usize>, i64) {
-    fn dfs(g: &Vec<Vec<(usize, i64)>>, root: usize) -> (Vec<usize>, Vec<i64>) {
-        let mut st = vec![root];
-        let n = g.len();
-        let mut parent = vec![n; n];
-        let mut dist = vec![0; n];
-        while let Some(u) = st.pop() {
-            for &(v, w) in g[u].iter() {
-                if v == parent[u] { continue; }
-                parent[v] = u;
-                dist[v] = dist[u] + w;
-                st.push(v);
-            }
+
+/// Monoid<S> is Commutative.
+/// map: F x S -> S is homomorphism.
+pub fn rerooting<S: Clone, F>(g: &Vec<(usize, usize, F)>, m: &Monoid<S>, map: Box<dyn Fn(&F, &S) -> S>) -> Vec<S> {
+    fn tree_dp<S, F>(
+        g: &Vec<Vec<(usize, &F)>>, 
+        dp: &mut Vec<S>, 
+        m: &Monoid<S>, 
+        map: &Box<dyn Fn(&F, &S) -> S>, 
+        u: usize,
+        parent: usize,
+    ) {
+        for &(v, x) in g[u].iter() {
+            if v == parent { continue; }
+            tree_dp(g, dp, m, map, v, u);
+            dp[u] = (m.op)(&dp[u], &map(x, &dp[v]));       
         }
-        (parent, dist)
     }
+    fn reroot<S: Clone, F>(
+        g: &Vec<Vec<(usize, &F)>>,
+        dp0: &Vec<S>,
+        dp1: &mut Vec<S>,
+        m: &Monoid<S>,
+        map: &Box<dyn Fn(&F, &S) -> S>,
+        u: usize,
+        parent: usize,
+    ) {
+        let mut childs = Vec::new();
+        for &e in g[u].iter() {if e.0 != parent { childs.push(e); }}
+        let deg = childs.len();
+        let mut dp_l = vec![(m.e)(); deg + 1];
+        let mut dp_r = vec![(m.e)(); deg + 1];
+        for (i, &(v, x)) in childs.iter().enumerate() {
+            dp_l[i + 1] = (m.op)(&dp_l[i], &map(x, &dp0[v]));
+        }
+        for (i, &(v, x)) in childs.iter().enumerate().rev() {
+            dp_r[i] = (m.op)(&dp_r[i + 1], &map(x, &dp0[v]));
+        }
+        for (i, &(v, x)) in childs.iter().enumerate() {
+            dp1[v] = map(x, &(m.op)(&dp1[u], &(m.op)(&dp_l[i], &dp_r[i + 1])));
+            reroot(g, dp0, dp1, m, map, v, u);
+        } 
+        
+    }
+    assert_eq!(m.commutative, true);
     let n = g.len() + 1;
-    let mut t: Vec<Vec<(usize, i64)>> = vec![vec![]; n];
-    for &(u, v, w) in g {
-        t[u].push((v, w));
-        t[v].push((u, w));
+    let mut t = vec![vec![]; n];
+    for (u, v, x) in g.iter() {
+        t[*u].push((*v, x));
+        t[*v].push((*u, x)); 
     }
-    let (_, dist) = dfs(&t, 0);
-    let u = dist.iter().enumerate().max_by_key(|(_, &d)| d).unwrap().0;
-    let (parent, dist) = dfs(&t, u);
-    let (mut v, &diameter) = dist.iter().enumerate().max_by_key(|&(v, d)| (d, v)).unwrap();
-    let mut path = Vec::new();
-    while v != n {
-        path.push(v);
-        v = parent[v];
+    let mut dp0: Vec<S> = vec![(m.e)(); n];
+    let mut dp1: Vec<S> = vec![(m.e)(); n];
+    tree_dp(&t, &mut dp0, m, &map, 0, n);
+    reroot(&t, &dp0, &mut dp1, m, &map, 0, n);
+    let mut dp = vec![(m.e)(); n];
+    for i in 0..n {
+        dp[i] = (m.op)(&dp0[i], &dp1[i]);
     }
-    (path, diameter)
+    dp
 }
