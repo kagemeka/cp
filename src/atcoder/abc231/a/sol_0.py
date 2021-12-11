e5d9988fea1ce86efa06c40359401668d0c22cc6
kagemeka <kagemeka1@gmail.com>
2021-12-12 00:43:52 +0900

    solve: ABC231 A-F(python)

---
+++ src/atcoder/abc231/a/sol_0.py
@@ -0,0 +1,7 @@
+import typing 
+
+def main() -> typing.NoReturn:
+    d = int(input())
+    print(d / 100)
+
+main()
---
+++ src/atcoder/abc231/c/sol_0.py
@@ -0,0 +1,15 @@
+import typing 
+import bisect 
+
+def main() -> typing.NoReturn:
+    n, q = map(int, input().split())
+    a = list(map(int, input().split()))
+    a.sort()
+    res = []
+    for _ in range(q):
+        x = int(input())
+        res.append(n - bisect.bisect_left(a, x))
+    print(*res, sep='\n')
+
+main()
+        
