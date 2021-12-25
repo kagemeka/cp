a9b81b8f328d4c7c60fc34703969815429a4d25a
kagemeka <kagemeka1@gmail.com>
2021-12-26 00:28:08 +0900

    solve: ABC233A-F

--- src/atcoder/agc043/b/sol_0.py
+++ src/atcoder/agc043/b/sol_0.py
@@ -1,32 +1,5 @@
 import typing 
 
-
-def base_convert_from_ten(
-    base: int, 
-    n: int,
-) -> typing.List[int]:
-    assert abs(base) >= 2
-    if n == 0: return [0]
-    digits = []
-    while n:
-        n, r = divmod(n, base)
-        if r < 0:
-            r -= base
-            n += 1
-        digits.append(r)
-    return digits
-
-
-def lucas_theorem(p: int, n: int, k: int) -> int:
-    r"""Lucas's Theorem.
-    references
-        - https://en.wikipedia.org/wiki/Lucas%27s_theorem
-    """
-    if k < 0 or n < k: return 0
-    a = base_convert_from_ten(p, n)
-    b = base_convert_from_ten(p, k)
-    ...
-    
     
     
 def main() -> typing.NoReturn:
@@ -34,8 +7,36 @@ def main() -> typing.NoReturn:
     a = list(map(int, input()))
     # odd, even
     a = [x - 1 for x in a]
+    k = 1 << 20
+    cnt2 = [0] * k
+    cnt2[2] = 1
+    for i in range(2, k):
+        if i * 2 >= k: break
+        cnt2[i * 2] = cnt2[i] + 1
+
+    for i in range(k - 1):
+        cnt2[i + 1] += cnt2[i] 
+
     def is_odd(a: typing.List[int]) -> bool:
         n = len(a)
-        ... 
+        bl = 0
+        for i in range(n):
+            # n - 1 choose i
+            # (n - 1)! / i! * (n - 1 - i)!
+            assert cnt2[n - 1] >= cnt2[i] + cnt2[n - 1 - i]
+            bl ^= (a[i] & 1) and cnt2[n - 1] == cnt2[i] + cnt2[n - 1 - i]
+        return bl
+
+    if is_odd(a):
+        print(1)
+        return 
+    if 1 in a:
+        print(0)
+        return 
+    a = [x // 2 for x in a]
+    if is_odd(a):
+        print(2)
+    else:
+        print(0)    
 
 main()
---
+++ src/atcoder/nomura2020/a/sol_0.py
@@ -0,0 +1,9 @@
+import typing 
+
+def main() -> typing.NoReturn:
+    a, b, c, d, k = map(int, input().split())
+    x = a * 60 + b
+    y = c * 60 + d
+    print(y - x - k)
+
+main()
