4c3eedcc92d6c8e63a5013f2ac66cc02c27f3571
kagemeka <kagemeka1@gmail.com>
2022-01-04 09:58:50 +0900

    solve: yorulatsu 1-3-2022

---
+++ src/atcoder/abc058/c/sol_1.py
@@ -0,0 +1,21 @@
+import typing 
+import collections
+import string
+
+def main() -> typing.NoReturn:
+    n = int(input())
+    cnt = collections.Counter()
+    for c in string.ascii_lowercase:
+        cnt[c] = 1 << 20
+    
+    for _ in range(n):
+        tmp = collections.Counter(input())
+        for c in cnt.keys():
+            cnt[c] = min(cnt[c], tmp[c])
+
+    t = ''
+    for c in string.ascii_lowercase:
+        t += c * cnt[c]
+    print(t)
+
+main()
