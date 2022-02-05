630e9b2359276c3be26784c65e08bd237947fe36
kagemeka <kagemeka1@gmail.com>
2022-02-05 20:31:08 +0900

    solve ABC237 A-E

---
+++ src/atcoder/abc237/b/sol_0.py
@@ -0,0 +1,18 @@
+import typing
+import numpy as np
+import sys
+
+
+def main() -> None:
+    h, w = map(int, input().split())
+    a = np.array(
+        sys.stdin.read().split(),
+        dtype=np.int64,
+    ).reshape(h, w)
+    a = a.T
+    for row in a.tolist():
+        print(*row)
+
+
+if __name__ == "__main__":
+    main()
